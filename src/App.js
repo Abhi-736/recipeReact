784b3252f95a2d2e974a7621540afc331e11f901
Harry <abhishekchauhan664@gmail.com>
2023-08-14 10:54:18 +0530

    add search option

--- src/App.js
+++ src/App.js
@@ -1,70 +1,139 @@
-import React from 'react';
-import './App.css';
-import { createContext,useState, useEffect, useContext } from 'react';
-import { AppProvider, userContext } from './context/AppProvider';
-
-
-
-
+import React from "react";
+import "./App.css";
+import { createContext, useState, useEffect, useContext } from "react";
+import { AppProvider, userContext } from "./context/AppProvider";
 
 function App() {
   return (
-
-
-
-<AppProvider>
-    <div className="App">
-      <Search/>
-      <Favourite/>
-      <Home/>
-    </div>
+    <AppProvider>
+      <div className="App">
+        <Search />
+        <Favourite />
+        <Home />
+      </div>
     </AppProvider>
   );
 }
 
 export default App;
 
-export const Search=()=>{
+export const Search = () => {
+  const [search, setSearch] = React.useState("");
+  const { fetchMeals, fetchRandom } = useContext(userContext);
 
-const [search, setSearch]= React.useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    search && fetchMeals(search);
+  };
 
-const {fetchData,fetchRandom}= useContext(userContext)
-
-const handleSubmit=(e)=>{
-e.preventDefault();
-search&&console.log(search)
-}
-
-return(
-
-<nav className="navbar bg-body-tertiary">
-  <div className="container-fluid">
-    <form className="d-flex" role="search" onSubmit={handleSubmit}>
-      <input className="form-control me-2" type="search" onChange={(e)=>setSearch(e.target.value)} placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success " type="submit">Search</button>
-      
-    </form>
-    <button className="btn btn-outline-danger ms-0" type="submit">Random</button>
-  </div>
-</nav>
-
-)
-
-}
-
-export const Favourite=()=>{return(
-<div className='Favourite'>
-  <div className='container-fluid'>{/* map */}
-    <div className='favourite-circle'>
-      <img src='' alt=''/>
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <form className="d-flex" role="search" onSubmit={handleSubmit}>
+          <input
+            className="form-control me-2"
+            type="search"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success " type="submit">
+            Search
+          </button>
+        </form>
+        <button
+          className="btn btn-outline-danger ms-0"
+          onClick={fetchRandom}
+          type="submit"
+        >
+          Random
+        </button>
+      </div>
+    </nav>
+  );
+};
+export const Favourite = () => {
+  return (
+    <div className="Favourite">
+      <div className="container-fluid">
+        {/* map */}
+        <div className="favourite-circle">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
-  </div>
-</div>)}
+  );
+};
 
-export const Home=()=>{
-  return(
-    <div className='container'>{/* map */}
+export const Home = () => {
+  const { Loading, MealArray, openMedalDescription, selectedMeal } =
+    useContext(userContext);
 
+  return (
+    <>
+      {Loading ? (
+        <Loader />
+      ) : (
+        <div className="container Home d-flex">
+          {MealArray.map((card) => {
+            return (
+              <div className="card">
+                <img
+                  src={card.strMealThumb}
+                  onClick={() => openMedalDescription(card)}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body d-flex">
+                  <p
+                    onClick={() => openMedalDescription(card)}
+                    className="card-text"
+                  >
+                    {card.strMeal}
+                  </p>
+                  <i
+                    onClick={() => {
+                      console.log("clicked i");
+                    }}
+                    class="bi bi-hand-thumbs-up"
+                  ></i>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {selectedMeal ? <MealDescription /> : null}
+    </>
+  );
+};
+export const Loader = () => {
+  return <div>Loading</div>;
+};
+
+export const MealDescription = () => {
+  const { selectedMeal } = React.useContext(userContext);
+
+  return (
+    <div className="MealInfo d-flex">
+      <div className="MealInfoContainer d-flex flex-column justify-center">
+        <img src={selectedMeal.strMealThumb} alt="MealThumb" />
+        <i className="bi bi-x-lg close"></i>
+        <div className="header">{selectedMeal.strMeal}</div>
+        <div className="description">{selectedMeal.strInstructions}</div>
+        <div className="sources d-flex ">
+          <button type="button" className="btn -btn-danger">
+            {" "}
+            Youtube
+          </button>
+          <button type="button" className="btn -btn-success">
+            Add To Favourite
+          </button>
+          <button type="button" className="btn -btn-success">
+            Source
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -1,30 +1,60 @@
-import React from 'react'
-import { createContext,useState, useEffect, useContext } from 'react';
+import React from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
-const userContext= createContext()
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
-const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
+const userContext = createContext();
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-const AppProvider = ({children}) => {
+const AppProvider = ({ children }) => {
+  const [MealArray, setMealArray] = React.useState([]);
+  const [Loading, setLoading] = React.useState(false);
+  const [selectedMeal, setselectedMeal]= React.useState('')
 
-    const [MealArray,setMealArray]= React.useState([])
+  const fetchMeals = (url) => {
+    setLoading(true);
+    let finalURL = `${allMealsUrl}${url}`;
+    fetch(finalURL)
+      .then((res) => res.json())
+      .then((data) => {
+        setMealArray(data.meals);
+        setLoading(false);
+      })
+      .catch((err) => {console.log(err);
+        setLoading(false);});
+    
+  };
 
-    const fetchData=(url)=>{
+  const fetchRandom = () => {
+    setLoading(true);
+    setMealArray([]);
+    fetch(randomMealUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setMealArray(data.meals);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+    
+  };
 
-        const finalURL=`${allMealsUrl}${url}`;
-        setMealArray=fetch(finalURL).then((res)=>{console.log(res)})
-    }
-
-const fetchRandom=()=>{
-
-
-}
+  const openMedalDescription=(card)=>{
+    setselectedMeal(card)
+console.log(card)
+  }
+  const contextStore = { 
+fetchMeals,
+  fetchRandom,
+  Loading,
+ MealArray,
+  selectedMeal,
+openMedalDescription };
 
   return (
-    <userContext.Provider value={{fetchData,fetchRandom}}>
-        {children}
-    </userContext.Provider>
-  )
-}
+    <userContext.Provider value={contextStore}>{children}</userContext.Provider>
+  );
+};
 
-export { AppProvider, userContext}
+export { AppProvider, userContext };
